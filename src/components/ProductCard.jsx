141072c424bfb86d5abdd1ee8f0d7ba3bf8fdf31
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-31 03:57:03 +0600

    fixed image

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,11 @@ function ProductCard({ product }) {
   return (
     <div>
       <div className="product-card">
-        <img src={product.image} className="product-card-image" />
+        <img
+          src={product.image}
+          alt={product.name}
+          className="product-card-image"
+        />
         <div className="product-card-content">
           <h3 className="product-card-name">{product.name}</h3>
           <p className="product-card-price">${product.price.toFixed(2)}</p>
